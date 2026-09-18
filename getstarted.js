f6f2a0503d6f5789cd8c5bb06fa07d6e7511a1fb
Ray Sage <kiran.co.press@gmail.com>
2026-09-18 17:31:55 +0200

    Update getstarted.js

--- getstarted.js
+++ getstarted.js
@@ -1,13 +1,11 @@
 // Finish account
 
-const name = localStorage.getItem("name");
-const email = localStorage.getItem("email");
 const imageInput = document.getElementById("imageIn");
 const preview = document.querySelector('.profilePic');
 const userName = document.querySelector('.userin');
 
-alert(name);
-userName.textContent = name + "!";
+
+
 
 
 imageInput.addEventListener('change', ()=>{
